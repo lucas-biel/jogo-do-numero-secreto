f24546d1f2a87228bb4ee3d196099ede2b5cbe64
Lucas <santos.lucas1029@gmail.com>
2025-01-27 16:14:13 -0300

    [Update]: aumentando a quantidade de números sorteados e adição de variável no texto.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let qtdNumSorteados = 10;
+let qtdNumSorteados = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 let mensagemDeAcerto;
@@ -25,7 +25,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${qtdNumSorteados}`);
 }
 
 exibirMensagemInicial();
